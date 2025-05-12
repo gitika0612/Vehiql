af4720f2f4de1469e27efdc3cddcc1103dcafb1f
gitika0612 <67464641+gitika0612@users.noreply.github.com>
2025-05-12 12:46:36 +0530

    header completed

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+
+const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/saved-cars(.*)', '/reservations(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+})
 
 export const config = {
   matcher: [
